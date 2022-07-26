4f2de44bb2270bb02d4885b074f0148c36072de1
kage1414 <kylejohnson92294@gmail.com>
2022-07-26 10:19:58 -0400

    Begin migration to goent framework

---
+++ ent/schema/name.go
@@ -0,0 +1,28 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// Name holds the schema definition for the Name entity.
+type Name struct {
+	ent.Schema
+}
+
+// Fields of the Name.
+func (Name) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("tconst"),
+		field.String("primaryName"),
+		field.Int("birthYear").
+			Positive(),
+		field.Int("deathYear").
+			Positive(),
+	}
+}
+
+// Edges of the Name.
+func (Name) Edges() []ent.Edge {
+	return nil
+}
